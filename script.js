a7577015229437322ee0054673bd21413c0ec5d4
Tanmoy Saha <tanmoytssaha@gmail.com>
2023-05-14 11:26:11 +0530

    Adding some functionality

---
+++ script.js
@@ -0,0 +1,44 @@
+const design_card_butttons = document.querySelectorAll('.design-card');
+const introduction_text = document.querySelectorAll('.introduction-text');
+
+const single_profile_card = document.querySelectorAll('.single-profile-card');
+const testimonial_card = document.querySelectorAll('.testimonial-card');
+
+design_card_butttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        introduction_text.forEach((introduction, introductionIndex) => {
+            if (index === introductionIndex) {
+                introduction.style.display = 'block';
+            } else {
+                introduction.style.display = 'none';
+            }
+        });
+        design_card_butttons.forEach((btn, btnIndex) => {
+            if (index === btnIndex) {
+                btn.classList.add('active');
+            } else {
+                btn.classList.remove('active');
+                
+            }
+        });
+    });
+});
+
+single_profile_card.forEach((btn, index) => {
+    btn.addEventListener('click', ()=> {
+        testimonial_card.forEach((testimonialCard, testimonialCardIndex) => {
+            if (index === testimonialCardIndex) {
+                testimonialCard.style.display = 'block';
+            } else {
+                testimonialCard.style.display = 'none';
+            }
+        });
+        single_profile_card.forEach((cardBtn, cardIndex) => {
+            if (index === cardIndex) {
+                cardBtn.classList.add('profile-card-active');
+            } else {
+                cardBtn.classList.remove('profile-card-active');
+            }
+        });
+    });
+});
